02da3e8a871cbb7c06841e67cae94dc973f6d41a
Justin <jatteberry@luc.edu>
2020-04-05 22:48:29 -0500

    -added 2 skeleton methods to main -added log class with skeleton methods

---
+++ src/Log.java
@@ -0,0 +1,36 @@
+//imports
+import java.io.File;
+
+public class Log {
+
+
+    public Log()
+    {
+      //TODO
+    }
+
+    //Returns T/F if file is made
+    public boolean createFile()
+    {
+      //TODO
+      return false;
+    }
+
+    //Returns T/F if file is written
+    public boolean writeFile()
+    {
+      //TODO
+      return false;
+    }
+
+    //Format for how to write the log
+    public String fileFormat()
+    {
+      //TODO
+      return null;
+    }
+
+
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 // imports
+import java.io.File;
+import java.util.Scanner;
 
 // the main program reads a file stream of input 'items' and adds them to our inventory, output a file
 public class Main {
 
     public static void main(final String[] args) {
+
+    }
+
+    public void checkInventoryIndex()
+    {
+        //TODO
+    }
+
+    public void readFile()
+    {
+        //TODO
     }
 }
