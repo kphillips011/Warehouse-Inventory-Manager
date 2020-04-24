cee885ee51238b1e5dbeffd595c50cd9ed6cdc17
Justin <jatteberry@luc.edu>
2020-04-24 13:22:47 -0500

    -Updated and working readFile with tests in main (added static Inventory in main to be able to add from both readFile and main)

--- src/Log.java
+++ src/Log.java
@@ -6,12 +6,13 @@ import java.util.Date;
 
 public class Log {
 
+
     //File name changes each time you create it with new date and time so no file will ever be the same. Allows you to
     //create more than one log file if needed without overwriting.
     public void writeFile()
     {
-        String fileName = new SimpleDateFormat("'log_'MM-dd-yyyy_hh-mm-ss'.md'").format(new Date());
-        File newFile = new File(fileName);
+        final String fileName = new SimpleDateFormat("'log_'MM-dd-yyyy_hh-mm-ss'.md'").format(new Date());
+        final File newFile = new File(fileName);
         //Not necessary unless we need to specify where the file is being created
         //newFile.getParentFile().mkdir(); Use for testing
         //TODO fill newFile with fileFormat()
--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,51 @@
 // imports
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 // the main program reads a file stream of input 'items' and adds them to our inventory, output a file
 public class Main {
 
     // TODO output log - two different logs that print inventory, maybe comparing the two for a changelog?
+    static Inventory inv = new Inventory(new HashMap<Integer, Product>());
 
     public static void main(final String[] args) {
+        inv.addProduct(new Product(1, "IPhoneSE", 399.99, 1));
+        String name = "inventoryIn.txt";
+        inv.getProductInfo(1);
+        inv.getProductInfo(2); //Should be non-existent
+        inv.getProductInfo(3); //Should be non-existent
+        readFile(name);
+        inv.getProductInfo(1); //Test to see if IPhoneSE just adds quantity of readFile to existing product
+        inv.getProductInfo(2); //Test to see if Non-existing product gets added in readFile
+        inv.getProductInfo(3);
+        inv.addItem(2, 3); //Test for adding to existing readFile product
+        inv.getProductInfo(2);
+
         /* Test for Log file creation (creates in IntelliJ Warehouse directory) (Works; Tested 4/13)
         Log newLog = new Log();
         newLog.writeFile();
         */
     }
 
-    public void checkInventoryIndex()
+    static void readFile(String fileName)
     {
-        //TODO
-    }
-
-    public void readFile()
-    {
+        final File file = new File(fileName);
         try {
-            File file = new File("//TODO");
-            Scanner fileReader = new Scanner(file);
-            //TODO read file input, add to inventory
+            final Scanner fileReader = new Scanner(file);
+
+            while (fileReader.hasNext()) {
+                int id = fileReader.nextInt();
+                String name = fileReader.next();
+                double price = fileReader.nextDouble();
+                int quantity = fileReader.nextInt();
+                if (inv.getMap().containsKey(id)) {
+                    inv.addItem(id, quantity);
+                } else {
+                    inv.addProduct(new Product(id,name,price,quantity));
+                }
+            }
             fileReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("No file found.");
